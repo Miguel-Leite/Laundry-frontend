ff685174575088fed5c61aad8c81c34c14aac384
Miguel Leite <miguelleite200leite@gmail.com>
2022-04-26 11:55:28 +0100

    styled done auth

--- src/pages/auth/styles.js
+++ src/pages/auth/styles.js
@@ -1,62 +1,131 @@
 import styled from "styled-components";
-import img from '../../assets/offers4.png';
 
 export const FormAuth = styled.div`
     width: 100%;
-    max-width: 100%;
-    height:100vh;
-    max-height: 100%;
-    form { 
-        width: 100%;
+    height:100%;
+    max-height: 100vh;
+    position: relative;
+    diplay: flex;
+
+    .imgBx {
+        position: relative;
+        width: 50%;
         height: 100%;
-        background: var(--form-ws);
-        padding: 20px 20px 60px 20px;
-        border-radius: 5px;
-        box-shadow: 0 0px 25px var(--light-ws);
-        label {
-            color: var(--label-login-ws);
-            margin-bottom: 15px;
-        }
-        div {
-            margin: 20px 0px;
-            input {
-                padding: 15px;
-                background-color: var(--input-login-ws);
-                border: none !important;
-                border-bottom: 1px solid transparent !important;
-                color: var(--dark-ws) !important;
-                transition: border .2s ease;
-                border-radius: 0 !important;
-                &:focus {
-                    background-color: var(--input-login-ws);
-                    box-shadow: none;
-                    border-bottom: 1px solid var(--button-login-ws) !important;
-                }
-                &:placeholder {
-                    color: var(--label-login-ws) !important;
-                }
-            }
-        }
-        button {
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
             width: 100%;
-            background-color: var(--button-login-ws);
-            color: var(--white-ws);
-            transition: filter .2s ease;
-            &:hover {
-                color: var(--light-ws);
-                filter: brightness(0.9);
-            }
+            height: 100%;
+            background: linear-gradient(225deg,var(--border-ws),var(--icon-ws));
+            z-index: 1;
+            mix-blend-mode: screen;
         }
-        div:first-of-type {
-            margin-bottom: 30px;
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+
         }
     }
-`;
+    .contentBx {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 100%;
 
-export const Logo = styled.div`
-    margin-top: 20%;
-    display: flex;
-    align-item: center;
-    justify-content: center;
-    margin-bottom: 20px;
+        .formBx {
+            width: 50%;
+
+            h2 {
+                color: var(--border-ws);
+                font-weight: 600;
+                font-size: 1.5em;
+                text-transform: uppercase;
+                margin-bottom: 20px;
+                border-bottom: 4px solid var(--border-ws);
+                display: inline-block;
+                letter-spacing: 1px;
+            }
+            .inputBx {
+                margin-bottom: 20px;
+                span {
+                    font-size: 16px;
+                    margin-bottom: 5px;
+                    display: inline-block;
+                    color: var(--border-ws);
+                    font-weight: 300;
+                    font-size: 16px;
+                    letter-spacing: 1px;
+                }
+                input {
+                    width: 100%;
+                    padding: 10px 20px;
+                    font-weight: 400;
+                    outline: none;
+                    border: 1px solid var(--input-border-ws);
+                    font-size: 16px;
+                    letter-spacing: 1px;
+                    color: var(--input-border-ws);
+                    background: transparent;
+                    border-radius: 30px;
+                }
+                input[type="submit"] {
+                    background-color: var(--border-ws);
+                    color: white;
+                    outline: none;
+                    border: none;
+                    font-weight: 500;
+                    cursor: pointer;
+                    transition: background-color .2s ease;
+                    &:hover {
+                        filter: brightness(.9);
+                    }
+                }
+
+                p {
+                    color: var(--border-ws);
+                    a {
+                        color: var(--icon-ws);
+                    }
+                }
+            }
+            .remember {
+                margin-bottom: 10px;
+                color: var(--border-ws);
+                font-weight: 400;
+                font-size: 14px;
+            }
+        }
+    }
+
+    @media(max-width: 768px) {
+        .imgBx {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+        .contentBx {
+            display: flex;
+            justify-content: center;
+            align-center: center;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+
+            .formBx {
+                width: 100%;
+                padding: 40px;
+                background: rgb(255 255 255 / 0.9);
+                margin: 50px;
+            }
+        }
+    }
 `;
